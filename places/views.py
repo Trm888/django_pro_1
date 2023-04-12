797b1f651a532d5f0b67309ced5fd9d22aaf1bac
tram888 <bigtram888@gmail.com>
2023-04-12 19:58:39 +0900

    Add API endpoint

--- places/views.py
+++ places/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from django.shortcuts import get_object_or_404
 
 from places.models import Place
 
+def place(request, place_id):
+    place = get_object_or_404(Place, pk=place_id)
+    return HttpResponse(place.title)
 
 def index(request):
     places = Place.objects.all()
