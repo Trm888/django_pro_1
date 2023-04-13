75bed9e3e206d2423587bab93b18d960fa7c716f
tram888 <bigtram888@gmail.com>
2023-04-13 20:42:05 +0900

    Added HTMLField

--- places/models.py
+++ places/models.py
@@ -1,14 +1,15 @@
 from django.core.validators import MinValueValidator
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField('Название', max_length=200)
     short_description = models.TextField('Краткое описание')
-    long_description = models.TextField('Полное описание')
     lat = models.FloatField('Широта')
     lon = models.FloatField('Долгота')
     place_id = models.CharField('ID места', max_length=200, unique=True, blank=True, null=True)
+    long_description = HTMLField('Полное описание')
 
     def __str__(self):
         return self.title
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from places import views
 
@@ -25,4 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('places/<int:place_id>/', views.get_json_place, name='place'),
+    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
